0959a5089e112143e52a8008a175a6dc4a2951b3
Akash146759 <akash.ganguly13@gmail.com>
2025-06-14 18:01:33 +0530

    2

--- Student table.sql
+++ Student table.sql
@@ -12,12 +12,3 @@ CREATE TABLE Students
 
 
 
-
-Select Name,Age from Students where Age >= 12;
-
-
-Select AVG(Age) as Average_Age from Students;
-
-Select SUM(Age) as total_age from Students;
-
-Select count(*) as total_records from Students;
